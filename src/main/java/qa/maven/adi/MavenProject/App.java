cbb82f4afbd81f9b11f0c3d2334ad364a4a99b91
Aditya <adimurti911@gmail.com>
2020-12-05 16:16:21 +0530

    first commit to learn Git

---
+++ src/main/java/qa/maven/adi/MavenProject/App.java
@@ -0,0 +1,13 @@
+package qa.maven.adi.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
